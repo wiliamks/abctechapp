23e27d1290874bd63a3d6e8ebc51a3569533eccd
Wiliam Otto <wiliam.keltke@gmail.com>
2022-10-20 23:41:16 -0300

    fix api url

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
-  //static const url = 'https://www.9aojo-abctech.dev.br';
+  static const url = 'https://www.9aojo-abctech.dev.br';
   //static const url = 'http://localhost:8080';
-  static const url = 'http://10.0.2.2:8080';
+  //static const url = 'http://10.0.2.2:8080';
 
   static const genericError = 'Erro na conexão';
 }
